a91c935f9750d8e2a7b3987589359673ceccbbfb
Pirisinu <palazzialessandro2@gmail.com>
2023-09-29 17:31:19 +0200

    [ADD]VUE.js list

--- js/scripit.js
+++ js/scripit.js
@@ -3,7 +3,25 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
+      tasks: [
+        {
+          cosaDaFare: 'Fare la spesa',
+          isDone: false
+        },
+        {
+          cosaDaFare: 'Portare il cane',
+          isDone: false
+        },
+        {
+          cosaDaFare: 'Andare alle poste',
+          isDone: false
+        },
+        {
+          cosaDaFare: 'Fare un viaggio',
+          isDone: false
+        }
 
+      ]
     }
   },
   methods:{
